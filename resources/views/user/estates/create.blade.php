f2b63766e8b11f88d7c36ceff833f846e847bcf5
FabrizioMisseri <misserifabrizio@gmail.com>
2023-02-08 21:14:55 +0100

    refactoring style in create form

--- resources/views/user/estates/create.blade.php
+++ resources/views/user/estates/create.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="container">
-        <h1 class="text-center">Inserisci la tua proprietà :0</h1>
+        <h1 class="text-center">Compila il form</h1>
         @if ($errors->any())
             <div class="alert alert-danger my-3" role="alert">
                 <ul>
@@ -14,16 +14,16 @@
                 </ul>
             </div>
         @endif
-                <form enctype="multipart/form-data" action="{{route('user.estates.store')}}" method="POST">
+        <form enctype="multipart/form-data" action="{{ route('user.estates.store') }}" method="POST">
             @csrf
             <div class="mb-3">
-              <label for="title" class="form-label">Titolo</label>
-              <input type="text" class="form-control" id="title" name="title">
+                <label for="title" class="form-label">Titolo</label>
+                <input type="text" class="form-control" id="title" name="title">
             </div>
 
             <div class="mb-3">
-              <label for="room_number" class="form-label">Numero di stanze</label>
-              <input type="text" class="form-control" id="room_number" name="room_number">
+                <label for="room_number" class="form-label">Numero di stanze</label>
+                <input type="text" class="form-control" id="room_number" name="room_number">
             </div>
 
             <div class="mb-3">
@@ -35,7 +35,7 @@
                 <label for="bathroom_number" class="form-label">Numero di bagni</label>
                 <input type="text" class="form-control" id="bathroom_number" name="bathroom_number">
             </div>
-            
+
             <div class="mb-3">
                 <label for="detail" class="form-label">Altri dettagli</label>
                 <input type="text" class="form-control" id="detail" name="detail">
@@ -48,36 +48,44 @@
                 <label for="cover_img" class="form-label">Immagine di copertina</label>
                 <input type="file" class="form-control" id="cover_img" name="cover_img">
             </div>
+
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="is_visible" name="is_visible">
+                <label class="form-check-label" for="is_visible">Spunta se vuoi renderlo visibile da subito</label>
+            </div>
+
             <div class="mb-3">
                 <select name="type" id="type">
                     <option value="">Scegli la tipologia di proprietà</option>
                     @foreach ($types as $type)
-                        <option value="{{$type}}">{{$type}}</option>
+                        <option value="{{ $type }}">{{ $type }}</option>
                     @endforeach
                 </select>
             </div>
 
-            <div class="mb-3 form-check">
-              <input type="checkbox" class="form-check-input" id="is_visible" name="is_visible" >
-              <label class="form-check-label" for="is_visible" >Seleziona se vuoi renderlo visibile da subito</label>
-            </div>
+            <div class="mb-3">
+                <div>Servizi:</div>
                 @foreach ($services as $service)
-                    <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input" id="service-{{$service->id}}" value="{{$service->id}}" name="services[]"    >
-                        <label class="form-check-label" for="service-{{$service->id}}" >{{$service->name}}</label>
+                    <div class="mb-1 form-check">
+                        <input type="checkbox" class="form-check-input" id="service-{{ $service->id }}"
+                            value="{{ $service->id }}" name="services[]">
+                        <label class="form-check-label" for="service-{{ $service->id }}">{{ $service->name }}</label>
                     </div>
                 @endforeach
+            </div>
+
             <div class="mb-3">
-                <label class="form-check-label" for="description" >Descrizione</label>
-                <textarea class="form-control" id="description" name="description"></textarea>    
+                <label class="form-check-label" for="description">Descrizione</label>
+                <textarea class="form-control" id="description" name="description"></textarea>
             </div>
             <div class="mb-3">
                 <label for="price" class="form-label">Inserisci Prezzo</label>
-                <input class="form-control w-25" type="number" min="0.01" step="0.01" max="3000" name="price" id="price"/>
+                <input class="form-control w-25" type="number" min="0.01" step="0.01" max="3000" name="price"
+                    id="price" />
             </div>
 
             <button type="submit" class="btn btn-primary">Inserisci proprietà</button>
-          </form>
+        </form>
 
     </div>
 @endsection
--- resources/views/user/estates/index.blade.php
+++ resources/views/user/estates/index.blade.php
@@ -4,39 +4,51 @@
 
 @section('content')
     <div class="container">
-        <h1 class="text-center">Ecco le tue proprietà</h1>
 
-    {{-- ciao --}}
+        {{-- “Hell is empty and all monsters are here.” --}}
+
+
+
+
         <div class="row justify-content-center mt-5">
-            <div class="col-12 col-md-10 col-lg-8">
+
+            <div class="col-12 col-md-10 col-lg-8 d-flex justify-content-between align-items-center">
+                <h1 class="text-center">Ecco le tue proprietà</h1>
+                <a href="{{ route('user.estates.create') }}" class="btn btn-success">
+                    inserisci proprietà
+                </a>
+            </div>
+
+            <div class="col-12 col-md-10 col-lg-8 mt-5">
                 <table class="table">
                     <thead>
-                    <tr>
-                        <th scope="col">Titolo</th>
-                        <th scope="col">Tipologia</th>
-                        <th scope="col">Mq</th>
-                        <th scope="col">Prezzo</th>
-                        <th scope="col">Visibile</th>
-                        <th scope="col">Azioni</th>
-                    </tr>
+                        <tr>
+                            <th scope="col">Titolo</th>
+                            <th scope="col">Tipologia</th>
+                            <th scope="col">Mq</th>
+                            <th scope="col">Prezzo</th>
+                            <th scope="col">Visibile</th>
+                            <th scope="col">Azioni</th>
+                        </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        @forelse ($estates as $estate)
-                            <tr>
-                                <th scope="row">{{$estate->title}}</th>
-                                <td>{{$estate->type}}</td>
-                                <td>{{$estate->mq}}</td>
-                                <td>{{$estate->price}}</td>
-                                <td>{{$estate->is_visible === 0 ? 'no':'si'}}</td>
-                                <td>
-                                    <a class="btn btn-success" href="{{route('user.estates.show',$estate->slug)}}">Scritta a caso</a>
-                                </td>
-                            </tr>
-                        @empty
-                            <tr>
-                                <th>Non sono ancora state caricate proprietà</th>
-                            </tr>
+                        <tr>
+                            @forelse ($estates as $estate)
+                        <tr>
+                            <th scope="row">{{ $estate->title }}</th>
+                            <td>{{ $estate->type }}</td>
+                            <td>{{ $estate->mq }}</td>
+                            <td>{{ $estate->price }}</td>
+                            <td>{{ $estate->is_visible === 0 ? 'no' : 'si' }}</td>
+                            <td>
+                                <a class="btn btn-success" href="{{ route('user.estates.show', $estate->slug) }}">Scritta a
+                                    caso</a>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <th>Non sono ancora state caricate proprietà</th>
+                        </tr>
                         @endforelse ($estates as $estate)
                     </tbody>
                 </table>
