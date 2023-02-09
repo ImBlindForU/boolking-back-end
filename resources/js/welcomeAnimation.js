2ad0b09c53f3f6c5a471c9334b0f039e8c8e1b8f
reistence <gvlamberti@gmail.com>
2023-02-09 14:45:47 +0100

    guard clause 4 welcome animation

--- resources/js/welcomeAnimation.js
+++ resources/js/welcomeAnimation.js
@@ -1,14 +1,19 @@
 const  firstTime = localStorage.getItem("first_time");
-if(!firstTime) {
-    // first time loaded!
-    console.log(1);
-    localStorage.setItem("first_time","1");
-    const timeline = gsap.timeline({defaults: {duration: 1, }});
-    timeline
-    .from("#nav-welcome",{ y: '-100%',ease: " Power4.easeOut",})
-    .from(".nav-link",{  opacity: 0, stagger: .6})
-    .from("#container-welcome",{  opacity: 0, stagger: .6})
-    .from("#logo-welcome", { opacity: 0, ease: "power2.in"}, 1)
-    .from("#h1-welcome", { x:  "-100%", ease: "power2.in"}, 1)
-    .from("#content-welcome", {  y: "-100%", ease: "power2.in"}, "<.5")
+console.log(window.location.pathname);
+
+if (window.location.pathname == "/") {
+    if(!firstTime) {
+        // first time loaded!
+        console.log(1);
+        localStorage.setItem("first_time","1");
+        const timeline = gsap.timeline({defaults: {duration: 1, }});
+        timeline
+        .from("#nav-welcome",{ y: '-100%',ease: " Power4.easeOut",})
+        .from(".nav-link",{  opacity: 0, stagger: .6})
+        .from("#container-welcome",{  opacity: 0, stagger: .6})
+        .from("#logo-welcome", { opacity: 0, ease: "power2.in"}, 1)
+        .from("#h1-welcome", { x:  "-100%", ease: "power2.in"}, 1)
+        .from("#content-welcome", {  y: "-100%", ease: "power2.in"}, "<.5")
+    }
+    
 }
