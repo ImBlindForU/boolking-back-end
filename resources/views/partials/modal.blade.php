7bd440248fe451b3cf691f287cde16eae88a6063
reistence <gvlamberti@gmail.com>
2023-02-12 16:23:33 +0100

    modal btn fixed

--- resources/views/partials/modal.blade.php
+++ resources/views/partials/modal.blade.php
@@ -6,7 +6,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">Annulla</button>
                 <button id="delete" type="button" class="btn btn-danger">Cancella</button>
             </div>
         </div>
