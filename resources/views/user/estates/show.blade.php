fa9fec3d644d7633d8eadc12c46494d5610fbb65
reistence <gvlamberti@gmail.com>
2023-02-12 13:25:32 +0100

    restyling show

--- resources/views/user/estates/show.blade.php
+++ resources/views/user/estates/show.blade.php
@@ -13,10 +13,10 @@
             <div class="col-12 col-lg-10">
                 <h2 class="text-center mb-4">{{ $estate->title }}</h2>
                 
-                <div class="mt-5 text-center"> 
+                <div class="mt-2 text-center"> 
                 {{--  IMAGE --}}
                 @if (str_contains($estate->cover_img, "cover")) 
-                <img class="rounded-3" src="{{ asset('storage/' . $estate->cover_img) }}" style="max-width: 500px">
+                <img class="rounded-3 w-100" src="{{ asset('storage/' . $estate->cover_img) }}" style="max-width: 500px">
                 @else 
                     <img src="{{$estate->cover_img}}" style="max-width: 500px">
         
@@ -24,7 +24,7 @@
                 {{-- / IMAGE --}}
                 </div>
 
-                <div id="img-show-div"  class="d-flex flex-wrap mt-5 align-center mx-auto justify-content-center">
+                <div id="img-show-div"  class="d-flex flex-wrap  my-4 align-center mx-auto justify-content-center">
                     @forelse ($estate->images as $image)
 
 
@@ -35,60 +35,52 @@
                     @empty
                         
                     @endforelse
+                </div>
+
+                <div id="info-wrapper" class="d-flex justify-content-between w-100">
+                <div class="street w-50">
+
+                    <p><span>Indirizzo:</span> {{ $estate->address?->street }}</p>
+                    <p><span>Numero Civico:</span> {{ $estate->address?->street_code }}</p>
+                    <p><span>Città:</span>{{ $estate->address?->city }} </p>
+                    <p><span>Paese:</span> {{ $estate->address?->country }}</p>
+                </div>
+
+                <div class="estate-details w-50 text-end">
+                    <p> <span>Tipologia:</span>{{ $estate->address?->type }} </p>
+                    <p><span>Nr. Stanze:</span> {{ $estate->address?->room_number }}</p>
+                    <p><span>Nr. Letti:</span>{{ $estate->address?->bed_number }} </p>
+                    <p><span>Nr. Bagni:</span> {{ $estate->address?->bathroom_number }}</p>
+                </div>
+                </div>
+                <div>
+                    <p><span>Dettagli:</span> {{ $estate->detail }}</p>
+                    <p><span>Prezzo:</span> {{ $estate->price }}</p>
+                    <p><span>Metri Quadri:</span> {{ $estate->mq }}</p>
+                    <p><span>Visibile:</span> {{ $estate->is_visible ? 'Si' : 'No' }}</p>
+                    <p><span>Descrizione:</span> {{ $estate->description}}</p>
 
                 </div>
 
 
-
-                <ul style="list-style:none">
-                    {{-- header --}}
-                    <li>
-                    </li>
-                    {{-- / header --}}
-                    {{-- Address --}}
-                    <li><strong>Indirizzo:</strong> {{ $estate->address?->street }}</li>
-                    <li><strong>Numero Civico:</strong> {{ $estate->address?->street_code }}</li>
-                    <li><strong>Città:</strong> {{ $estate->address?->city }}</li>
-                    <li><strong>Paese:</strong> {{ $estate->address?->country }}</li>
-                    {{-- /Address --}}
-
-                    {{-- features --}}
-                    <li><strong>Tipologia:</strong> {{ $estate->type }}</li>
-                    <li><strong>Stanze:</strong> {{ $estate->room_number }}</li>
-                    <li><strong>Letti:</strong> {{ $estate->bed_number }}</li>
-                    <li><strong>Bagni:</strong> {{ $estate->bathroom_number }}</li>
-                    <li><strong>Dettagli:</strong> {{ $estate->detail }}</li>
-                    <li><strong>Prezzo:</strong> {{ $estate->price }}</li>
-                    <li><strong>Metri quadri:</strong> {{ $estate->mq }}</li>
-                    <li><strong>Visibile:</strong> {{ $estate->is_visible ? 'si' : 'no' }}</li>
-                    {{-- / features --}}
-
-                    {{-- description text --}}
-                    <li class="mb-4 mt-4">
-                        <strong>descrizione:</strong><br>
-                        {{ $estate->description }}
-                    </li>
-                    {{-- / description text --}}
+                <dl style="">
+                    <dt>Servizi aggiuntivi:</dt>
 
                     {{-- services --}}
-                    <li><strong>Servizi aggiuntivi:</strong></li>
                     @forelse ($estate->services as $service)
-                        <li>{{ $service->name }}</li>
+                        <dl>{{ $service->name }}</dl>
                     @empty
-                        <li>
+                        <dl>
                             Nessun servizio specificato
-                        </li>
+                        </dl>
                     @endforelse
                     {{-- / services --}}
-
-                  
-                
-                    {{-- index btn --}}
-                    <li class="mt-5 mb-5">
-                        <a class="btn our-btn px-3" href="{{ route('user.estates.index') }}">Proprietà</a>
-                    </li>
-                    {{-- / index btn --}}
-                </ul>
+                </dl>
+                {{-- index btn --}}
+                <div class="mt-5 mb-5">
+                    <a class="btn our-btn px-3" href="{{ route('user.estates.index') }}">Proprietà</a>
+                </div>
+                {{-- / index btn --}}
             </div>
             {{-- / SINGLE ESTATE --}}
 
