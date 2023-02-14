50d0f6108434db4e561a8c3adacbe86e6d41248e
BetterCallAle <dap22091998@gmail.com>
2023-02-14 17:41:57 +0100

    Add: api for services

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\EstateController;
+use App\Http\Controllers\Api\ServiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('estates', [EstateController::class, 'index']);
+Route::get('services', [ServiceController::class, 'index']);
