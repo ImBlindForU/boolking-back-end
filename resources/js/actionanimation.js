0a8d73b5bbe1b9a31ec6365a971c48a9b8eff6c2
ImBlindForU <samuele.zanchi10@gmail.com>
2023-02-17 18:02:19 +0100

    comment js

--- resources/js/actionanimation.js
+++ resources/js/actionanimation.js
@@ -8,4 +8,21 @@ queryAll.forEach((query, index) => {
     queryAllptDue[index].classList.toggle('active');
     });
 });
-    
+
+// const queryName = document.querySelectorAll('.active')
+// console.log(queryName);
+// if (window.location.pathname == "/user/estates") {
+//     const body = document.querySelector('body');
+//     body.addEventListener('click', function () {
+//         console.log('fuori')
+        
+//         if(queryName.length > 0){
+//             queryAllptDue.forEach(query => {
+//             query.classList.remove('active')
+
+//             console.log('dentro')
+
+//        });
+//     }
+//     });
+// }
