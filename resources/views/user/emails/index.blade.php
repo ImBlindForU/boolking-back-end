a85ef011ff1eadbc0ea11d21c90b87aef8e81624
FabrizioMisseri <misserifabrizio@gmail.com>
2023-02-20 17:29:53 +0100

    add table in messages-index

--- resources/views/user/emails/index.blade.php
+++ resources/views/user/emails/index.blade.php
@@ -5,20 +5,56 @@
         <div class="container mt-4">
             <h3 class="text-center text-danger fw-bold">Messaggi:</h3>
             <div class="row justify-content-center">
-                <div class="col-11">
-                    @if (session('message'))
-                        <div class="alert alert-success">
-                            {{ session('message') }}
-                        </div>
-                    @endif
-                    <ul>
-                        @foreach ($leads as $lead)
-                            @foreach ($lead as $item)
-                                <li>{{ $item['message'] }}</li>
-                            @endforeach
+
+                {{-- TABLE --}}
+                <table class="table table-hover">
+
+                    {{-- HEAD --}}
+                    <thead>
+                        <tr class="text-center">
+                            <th scope="col" class="pe-4">Nome</th>
+                            <th scope="col" class="pe-4">Mail</th>
+                            <th scope="col" class="pe-4">Proprietà</th>
+                            <th scope="col">Messaggio</th>
+                        </tr>
+                    </thead>
+                    {{-- / HEAD --}}
+
+                    {{-- BODY --}}
+                    <tbody>
+                        <tr></tr>
+
+                        @foreach ($estates as $estate)
+                            @forelse ($estate->leads as $lead)
+                                <tr>
+                                    <td class="pe-4">
+                                        {{ $lead->name }}
+                                    </td>
+                                    <td class="pe-4">
+                                        {{ $lead->email }}
+                                    </td>
+                                    <td class="pe-4">
+                                        {{ $estate->title }}
+                                    </td>
+                                    <td>
+                                        {{ $lead->message }}
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <th>
+                                        NON CI SONO MESSAGGI
+                                    </th>
+                                </tr>
+                            @endforelse
                         @endforeach
-                    </ul>
-                </div>
+
+                    </tbody>
+                    {{-- / BODY --}}
+
+                </table>
+                {{-- / TABLE --}}
+
             </div>
         </div>
     </div>
